b37be0bb6a6112161c446c68fded9954a3431752
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2020-06-01 17:08:02 -0400

    tableau associatif (Map)

---
+++ src/tableau_associatif.ts
@@ -0,0 +1,12 @@
+let maMap = new Map();
+maMap.set(77,'Poisson');
+maMap.set(22,'Citron');
+console.log(maMap); // Map { 77 => 'Poisson', 22 => 'Citron' }
+
+maMap.has(77);    // true
+maMap.delete(22); // true
+
+console.log(maMap); // Map { 77 => 'Poisson' }
+
+console.log(maMap.get(77)); // Poisson
+console.log(maMap.get(22)); // undefined
