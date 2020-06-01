c1a658caa9f02a35d2f30753f4789a29aa090b3a
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2020-06-01 16:04:36 -0400

    initial commit

---
+++ src/get_students_fetch.ts
@@ -0,0 +1,60 @@
+import md5 = require('md5');
+import fetch = require('node-fetch');
+
+const teacherId: string = 'teacher+3@gmail.com';
+const urlServeurSGB: string = 'http://localhost:3001';
+const endPointGroupeCoursEnseignant: string = '/api/v1/courses';
+
+class GroupeCours {
+    id: number;
+    sigle: string;
+    maxNbÉtudiants: number;
+    groupe: string;
+    titre: string;
+    dateDébut: string;
+    dateFin: string;
+
+    constructor(sigle: string, titre: string, nbMaxÉtudiants: number, groupe: string, dateDébut: string, dateFin: string,
+        id: number) {
+        this.id = id;
+        this.sigle = sigle;
+        this.maxNbÉtudiants = nbMaxÉtudiants;
+        this.groupe = groupe;
+        this.titre = titre;
+        this.dateDébut = dateDébut;
+        this.dateFin = dateFin;
+    }
+}
+
+function formatGroupeCours(groupeCoursRéponseSGB: any): GroupeCours {
+    return {
+        id: groupeCoursRéponseSGB.id,
+        sigle: groupeCoursRéponseSGB.sigle,
+        maxNbÉtudiants: groupeCoursRéponseSGB.nb_max_student,
+        groupe: groupeCoursRéponseSGB.groupe,
+        titre: groupeCoursRéponseSGB.titre,
+        dateDébut: groupeCoursRéponseSGB.date_debut,
+        dateFin: groupeCoursRéponseSGB.date_fin
+    };
+}
+
+class ServiceGroupeCours {
+    getGroupeCours(teacherId: string): Promise<GroupeCours[]> {
+        return fetch(urlServeurSGB + endPointGroupeCoursEnseignant, { headers: { token: md5(teacherId) } })
+            .then((res) => res.json())
+            .then((res) => res.data.map((groupeCoursSGB: any) => formatGroupeCours(groupeCoursSGB)))
+            .catch(function (err: any) {
+                return console.log(err);;
+            });
+    }
+}
+
+let apiClient = new ServiceGroupeCours();
+
+apiClient.getGroupeCours(teacherId)
+    .then((groupeCoursTableau) => {
+        console.log("Cours pour " + teacherId);
+        return groupeCoursTableau.map(function (gc) {
+            return console.log('Cours: "' + gc.sigle + ': ' + gc.titre + '" g' + gc.groupe);
+        });
+    });
