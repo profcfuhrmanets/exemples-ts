819b38b24b16dcd38e4deec01882bbfc1baaf326
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-08-30 10:26:04 -0400

    formatage, maj express

--- src/http_methods/Employe.ts
+++ src/http_methods/Employe.ts
@@ -1,32 +1,32 @@
 export class Employe {
-    private _nom:string;
-    private _poste:string;
-    private _tauxHoraire:number;
+    private _nom: string;
+    private _poste: string;
+    private _tauxHoraire: number;
 
-    constructor(nom:string, poste:string, tauxHoraire:number) {
+    constructor(nom: string, poste: string, tauxHoraire: number) {
         this._nom = nom;
         this.poste = poste;
         this.tauxHoraire = tauxHoraire;
     }
 
     // Lecture seule
-    get nom():string {
+    get nom(): string {
         return this._nom;
     }
 
-    get poste():string {
+    get poste(): string {
         return this._poste;
     }
 
-    set poste(poste:string) {
+    set poste(poste: string) {
         this._poste = poste;
     }
 
-    get tauxHoraire():number {
+    get tauxHoraire(): number {
         return this._tauxHoraire;
     }
 
-    set tauxHoraire(tauxHoraire:number) {
+    set tauxHoraire(tauxHoraire: number) {
         this._tauxHoraire = tauxHoraire;
     }
 
