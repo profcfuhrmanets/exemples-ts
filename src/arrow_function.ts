49ecba2c84d94a62ee34b30127634c13c7903613
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-08-30 10:32:08 -0400

    Lien vers tutoriel, return pour la cohérence

--- src/arrow_function.ts
+++ src/arrow_function.ts
@@ -1,10 +1,9 @@
+// https://fr.javascript.info/arrow-functions-basics
 function hello(qui: string) {
-    console.log("Hello " + qui);
+    return console.log("Hello " + qui);
 }
 
 const bonjour = (qui: string) => console.log("Bonjour " + qui)
 
 hello("world");
 bonjour("monde");
-
-// const bonjour1 = (qui: string) => return console.log("Bonjour " + qui)
